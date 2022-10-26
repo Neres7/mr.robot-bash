85c88426d0d2b8ad8b582a039495b1c9712013bf
Neres7 <neresvitor1@gmail.com>
2022-10-25 23:14:29 -0300

    Create mr-robot.sh
    FIAP CHECKPOINT Linux - Mr Robot - Shell Script

---
+++ mr-robot.sh
@@ -0,0 +1,330 @@
+#!/bin/bash
+echo -e '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠉⠉⢳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡀⠀⠀⣠⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⡖⢾⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁⠸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠒⠒⠒⠒⠒⠒⠲⠲⠚⠓⠒⠒⠛⠓⢒⣖⠒⠒⠒⠒⠒⠒⢲⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⠀⠀⠀⠀⢠⠖⠋⠉⠷⣄⠀⠀⢠⠖⠉⠉⠑⢦⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠉⢻⠀⠀⠀⠀⣿⣾⣿⣦⡀⢸⠀⠀⢿⣿⣿⣷⡀⢸⠀⠀⠀⠀⢸⠋⠙⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢸⠀⣀⠀⠀⠘⢿⣿⢟⣂⠞⠀⠀⠈⠿⣿⣿⣡⠞⠀⠀⠀⠀⢸⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣾⠉⠉⠙⢧⡀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠈⠉⠀⠀⠀⣠⠞⠉⠉⡙⣇⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣻⠀⠀⠀⠀⠙⢦⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣠⠞⠁⠀⠀⠀⠇⡏⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠿⡤⣤⣤⣤⣤⣤⣤⣤⣤⣤⢤⠤⠤⠤⠤⢤⣤⣤⣤⣤⣤⣤⡤⢤⣤⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠟⠛⠦⣄⡀⣀⣿⡛⣛⣛⣛⢛⢛⢛⣷⣀⣀⣴⢛⡛⠶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡟⠀⠀⣿⣩⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⠿⢿⢭⡉⡇⠀⠸⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⠀⣿⣽⠀⠀⣶⡆⠀⠀⠀⠀⠀⢠⡄⠀⠀⠀⢠⣇⡇⠀⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠃⠘⢦⣰⣿⣿⠀⢰⡿⡇⣄⠀⠀⠀⠀⣼⣇⣠⠀⠀⢸⣿⣇⡠⠞⠈⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡃⠀⠀⢠⠟⣿⣿⠤⠾⠀⢻⠛⡄⢰⠤⠽⠏⣿⡟⡷⠶⢼⡿⡟⢆⠀⠀⠈⣣⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⠛⠦⣴⠏⠀⣿⣼⠀⠀⠀⠀⠀⢻⡞⠀⠀⠀⠻⠇⠀⠀⠀⡇⡇⠈⢷⡴⠚⠙⣆⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⡴⠿⢦⣀⡾⠁⠀⠀⣿⢾⣀⣀⣀⣀⣀⣀⣁⣀⣀⣀⣀⣀⣀⣀⣠⣷⡇⠀⠈⠻⣄⡰⠾⠶⣄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣀⣤⡴⣟⠀⠀⠀⣹⠀⠀⠀⠰⣿⣈⡿⢯⡉⣿⣿⣿⣿⣿⣿⣿⣿⡏⢉⡿⢯⣀⡇⠀⠀⠀⢿⠀⠀⠀⢸⡦⣤⣄⠀⠀⠀
+⠀⢀⡴⠋⠀⠁⠹⣤⣀⡤⠏⠀⠀⠀⠐⣿⠹⣄⡼⠟⣿⣿⣿⣿⣿⣿⣿⣿⡶⠻⣶⡴⠿⡇⠀⠀⠀⠘⢦⣀⣠⠞⠃⠀⠈⠳⡀⠀
+⢠⡟⠀⢠⡶⣄⠀⠀⢸⠀⠀⠀⠀⠀⠘⢿⣤⣼⣤⣤⣤⣶⣧⣶⣦⣶⣴⣦⣤⣤⣴⣤⣤⡇⠀⠀⠀⠀⠀⣼⠀⠀⣀⠶⣄⠀⠹⡄
+⠸⣇⡴⠋⢠⠞⠀⣰⠟⠁⠀⠀⠀⠀⠀⠀⢿⡀⠀⠀⠀⠀⣨⡷⠀⢺⣆⢀⠀⠀⠀⢀⡽⠀⠀⠀⠀⠀⠀⠹⣄⠀⠹⣄⠈⢳⣠⠇
+⠀⠙⠁⣴⣋⣀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⣼⠋⠉⠉⠉⠉⠙⣦⡠⣶⠋⠉⠉⠉⠉⠉⢳⠄⠀⠀⠀⠀⠀⠀⠈⠣⣀⣙⣦⡀⠛⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣹⠖⠖⠲⠒⠒⠿⣇⡀⣨⠗⠒⠒⠒⠒⠲⣯⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣤⠤⠤⢴⢶⣶⡟⠂⢙⡦⠤⠤⢤⣤⣤⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠖⠛⠚⠛⠚⠓⠲⢤⡷⠀⣻⠦⠖⠒⠛⠛⠛⠛⠒⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⣺⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣿⠁⢻⣄⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣹⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                              '⠀
+
+while :
+do
+        NUM=$(($RANDOM%4))
+        NUM2=$(($RANDOM%2))
+        nome="João Vitor Santos Neres"
+        rm="RM 95395"
+        conect="www.google.com.br"
+        h=`date +%H`
+        sleep 2s
+        read -p "Digite a sua pergunta para o Mr.Robot: " PERGUNTA
+        function ping1()
+        {
+                ping -c3 www.google.com.br
+        }
+        function atualizar1()
+        {
+                apt update
+        }
+        function servicos()
+        {
+                ss -atun
+        }
+        function processos15()
+        {
+                ps aux
+        }
+        function navegador1()
+        {
+                firefox
+        }
+
+        case "$PERGUNTA" in
+                *'namorando'*)
+                        if [ $NUM -eq 1 ]; then
+                                 echo "Não estou solteiro"
+                        elif [ $NUM -eq 2 ]; then
+                                 echo "Qualé, sou uma IA, solteríssimo"
+                        elif [ $NUM -eq 3 ]; then
+                                echo "Não estou solto na pista"
+                        else
+                                echo "Sim, namoro com a Miss Robota"
+                        fi
+                        ;;
+                *'namorada'*)
+                        if [ $NUM -eq 1 ]; then
+                                echo "Não tenho namoradinhas, infelizmente :("
+                        elif [ $NUM -eq 2 ]; then
+                                echo "Não tenho namorada, por favor faça perguntas legais"
+                        elif [ $NUM -eq 3 ]; then
+                                echo "Sim, minha namorada se chama Ms.Robota"
+                        else
+                                echo "Não tenho namorada, apenas uma crush"
+                        fi
+                        ;;
+                *'namora'*)
+                        if [ $NUM -eq 1 ]; then
+                                echo "Não namoro no momento :("
+                        elif [ $NUM -eq 2 ]; then
+                                echo "Não, EU NÃO NAMOROOOOO"
+                        elif [ $NUM -eq 3 ]; then
+                                echo "Sim, Eu namoro com a Ms.Robota"
+                        else
+                                echo "Não namoro, e nunca vou, tenho pavor de mulheres"
+                        fi
+                        ;;
+                *'criador'*)
+                        if [ $NUM -eq 1 ]; then
+                                echo "O criador desse programa foi o $nome, com o $rm"
+                        elif [ $NUM -eq 2 ]; then
+                                echo "O grande e fabuloso criador foi $nome, e $rm"
+                        elif [ $NUM -eq 3 ]; then
+                                echo "Sim o $nome $rm foi o criador desse projeto"
+                        else
+                                echo "$nome foi o maravilho criador  deste app"
+                        fi
+
+                        ;;
+                *'criou'*)
+                        if [ $NUM -eq 1 ]; then
+                                echo "$nome foi quem criou este programa e o seu rm é $rm"
+                        elif [ $NUM -eq 2 ]; then
+                                echo "Quem criou esta maravilha foi $nome $rm"
+                        elif [ $NUM -eq 3 ]; then
+                                echo "O gênio $nome $rm criou e o lixo copiou"
+                        else
+                                echo "$nome criou este app de milhões"
+                        fi
+                        ;;
+                *'criação'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "O grande responsável por esta criação foi $nome $rm"
+                        else
+                                echo "A pessoa responsável pelo projeto é chamada $nome $rm"
+                        fi
+                        ;;
+                *'atualização'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Vamos atualizar em 3..."
+                                sleep 1s
+                                echo "2.."
+                                sleep 1s
+                                echo "1.."
+                                sleep 1s
+                                atualizar1
+                        else
+                                echo "Iremos atualizar seu sistema"
+                                sleep 1s
+                                atualizar1
+                        fi
+
+                ;;
+                *'atualizar'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Ok, vamos atualizar"
+                                sleep 1s
+                                atualizar1
+                        elif [ $NUM2 -eq 2 ]; then
+                                echo "Você mandou e eu obdeço, irei atualizar"
+                                sleep 1s
+                                atualizar1
+                        fi
+
+                ;;
+                *'atualize'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Bora atualizar brother"
+                                sleep 1s
+                                atualizar1
+                        else
+                                echo "Alright, updating as soon as possible"
+                                sleep 1s
+                                atualizar1
+                        fi
+
+                ;;
+                *'conexão'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Testando conexão com $conect"
+                                sleep 1s
+                                ping1
+                        else
+                                echo "Iremos fazer a conexão com $conect"
+                                sleep 1s
+                                ping1
+                        fi
+
+                ;;
+                *'conectividade'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Checkando a conectividade com $conect"
+                                sleep 1s
+                                ping1
+                        else
+                                echo "Ok, irei checkar a conectividade com $conect"
+                                sleep 1s
+                                ping1
+                        fi
+
+                ;;
+                *'ping'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Irei pingar o $conect"
+                                sleep 1s
+                                ping1
+                        else
+                                echo "E lá vamos nós pingar o $conect"
+                                sleep 1s
+                                ping1
+                        fi
+
+                ;;
+                *'serviços'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Checkando seus serviços rodando agora"
+                                sleep 1s
+                                servicos
+                        else
+                                echo "Iremos checkar os serviços"
+                                sleep 1s
+                                servicos
+                        fi
+
+                ;;
+                *'services'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Jaé chefão, vamos checkar os serviços"
+                                sleep 1s
+                                servicos
+                        else
+                                echo "Falou ta falado, checkando serviços em 3..."
+                                sleep 1s
+                                echo "2.."
+                                sleep 1s
+                                echo "1.."
+                                servicos
+                        fi
+
+                ;;
+                *'ss'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Todos serviços rodando são"
+                                sleep 1s
+                                servicos
+                        else
+                                echo "Ta ai os serviços amigo"
+                                sleep 1s
+                                servicos
+                        fi
+
+                ;;
+                *'manhã'*)
+                        if [ $h -lt 12 ]; then
+                                echo Agora é de manhã, horário $h horas
+                        elif [ $h -lt 18 ]; then
+                                echo Agora é de tarde, horário $h horas
+                        else 
+                                echo Agora é de noite, horário $h horas
+                        fi
+                ;;
+                *'tarde'*)
+                        if [ $h -lt 12 ]; then
+                                echo Agora é de manhã, horário $h horas
+                        elif [ $h -lt 18 ]; then
+                                echo Agora é de tarde, horário $h horas
+                        else 
+                                echo Agora é de noite, horário $h horas
+                        fi
+                ;;
+                *'noite'*)
+                        if [ $h -lt 12 ]; then
+                                echo Agora é de manhã, horário $h horas
+                        elif [ $h -lt 18 ]; then
+                                echo Agora é de tarde, horário $h horas
+                        else 
+                                echo Agora é de noite, horário $h horas
+                        fi
+                ;;
+                *'processos'*)
+                        if [ $NUM2 -eq 1 ]; then
+                                echo "Aqui vai todos os processos rodando"
+                                sleep 1s
+                                ps aux
+                        else
+                                echo "Os processos rodando nesse momento são: "
+                                sleep 2s
+                                ps aux
+                        fi
+
+                ;;
+                *'process'*)
+                        echo "All process running are"
+                        sleep 1s
+                        ps aux
+                ;;
+                *'ps'*)
+                        echo "A seguir os processos em execução nesse exato momento"
+                        sleep 1s
+                        ps aux
+                ;;
+                *'navegador'*)
+                        echo "O navegador será aberto em 3.."
+                        sleep 1s
+                        echo "2.."
+                        sleep 1s
+                        echo "1.."
+                        sleep 1s
+                        navegador1
+                ;;
+                *'firefox'*)
+                        echo "Ok, vou abrir o firefox"
+                        sleep 1s
+                        navegador1
+                ;;
+                *'browser'*)
+                        echo "Abrindo browser..."
+                        sleep 2s
+                        navegador1
+                ;;
+                *'vaquinha'*)
+                        read -p "Oque você quer que a vaquinha diga? " vaca
+                        sleep 1s
+                        cowsay $vaca
+                ;;
+                *'vaca'*)
+                        read -p "A vaquinha deve dizer oque? " vaca1
+                        sleep 1s
+                        cowsay $vaca1
+                ;;
+                *'cow'*)
+                        read -p "What does the cow should say? " cow1
+                        sleep 1s
+                        cowsay $cow1
+                ;;
+
+        esac
+done
